29f30d967a24e3dd3303c709dac9d95e56295816
BermetKalmakova <bermetkalmakova@gmail.com>
2016-11-30 20:43:58 -0500

    Update ReferenceBook

--- 01Library/ReferenceBook.java
+++ 01Library/ReferenceBook.java
@@ -2,7 +2,7 @@ public class ReferenceBook extends LibraryBook{
     private String collection;
 
     public ReferenceBook(String auth, String titl, String isbnum, String callNum, String collect){
-	LibraryBook(titl, auth, isbnum, callNum);
+        super(titl,auth,isbnum,callNum);
 	collection = collect;
     }
     
@@ -15,4 +15,13 @@ public class ReferenceBook extends LibraryBook{
     public void checkout(String patron, String due){
 	System.out.println("Cannot check out a reference book.");
     }
+    public void returned(){
+	System.out.println("Reference book could not have been checked out -- return impossible");
+    }
+    public String circulationStatus(){
+	return "non-circulating reference book";
+    }
+    public String toString(){
+	return super.toString() + "\nCollection: " + collection;
+    }  
 }
