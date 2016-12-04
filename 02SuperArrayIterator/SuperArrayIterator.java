652a871b93bb59bd91df2de8b3a77053deffac0e
BermetKalmakova <bermetkalmakova@gmail.com>
2016-12-04 16:02:28 -0500

    Update SuperArrayIterator

--- 02SuperArrayIterator/SuperArrayIterator.java
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -1,1 +1,24 @@
-public class 
+import java.util.*;
+public class SuperArrayIterator implements Iterator<String>{
+    private SuperArray x;
+    private int i;
+    public SuperArrayIterator(SuperArray a){
+	x = a;
+	i = 0;
+    }
+    public boolean hasNext(){
+	return i < x.size();
+    }
+    public String next(){
+	if(hasNext()){
+	    i ++;
+	    return x.get(i-1);
+	}else{
+	    throw new NoSuchElementException();
+	}
+    }
+
+    public void remove(){
+	throw new UnsupportedOperationException();
+    }
+}
