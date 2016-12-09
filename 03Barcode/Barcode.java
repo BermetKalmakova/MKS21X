58f93e339877737216b487396a6bc8cbb6cd38f2
BermetKalmakova <bermetkalmakova@gmail.com>
2016-12-09 03:38:42 -0500

    Update Barcode

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,13 +1,7 @@
 public class Barcode implements Comparable<Barcode>{
-    // instance variables
     private String _zip;
-    private int _checkDigit;
+    private static String[] bars = {"||:::", ":::||", "::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::"}
 
-    // constructors
-    //precondtion: _zip.length() = 5 and zip contains only digits.
-    //postcondition: throws a runtime exception zip is not the correct length
-    //               or zip contains a non digit
-    //               _zip and _checkDigit are initialized.
     public Barcode(String zip) {
 	if (zip.length() != 5){
 	    throw new IllegalArgumentException();
@@ -19,49 +13,35 @@ public class Barcode implements Comparable<Barcode>{
 
 	}
 	_zip = zip;
-	_checkDigit = checkSum();
-    }
-
-    // postcondition: Creates a copy of a bar code.
-    public Barcode clone(){
-        Barcode x = new Barcode(_zip);
-	return x;
     }
 
 
     // postcondition: computes and returns the check sum for _zip
-    private int checkSum(){
+    private int checkSum(String a){
 	int sum = 0;
 	for(int i = 0;i<5;i++){
-	    sum+=(int)_zip.charAt(i);
+	    sum+=(int)a.charAt(i);
 	}
 	return sum%10;
     }
 
-    private String toBarcode(){
+    private static String toCode(String zip){
 	String ans = "";
-	String x = _zip + _checkDigit;
-	ans = x.replace("1", ":::||");
-	ans = x.replace("2", "::|:|");
-	ans = x.replace("3", "::||:");
-	ans = x.replace("4", ":|::|");
-	ans = x.replace("5", ":|:|:");
-	ans = x.replace("6", ":||::");
-	ans = x.replace("7", "|:::|");
-	ans = x.replace("8", "|::|:");
-	ans = x.replace("9", "|:|::");
-	ans = x.replace("0", "||:::");
+	String x = zip + checkSum(zip);
+	for(int i = 0;i<6;i++){
+	    ans += bars.get((int)x.charAt(i));
+	}
 	return ans;
     }
-    
-    //postcondition: format zip + check digit + Barcode 
-    //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
+      
     public String toString(){
-	return _zip + _checkDigit + " |" + toBarcode() + "|";
+	return _zip + checkSum(_zip) + " |" + toCode() + "|";
     }
 
+    public static String toZip(String code){
+	
+    }
 
-    // postcondition: compares the zip + checkdigit, in numerical order. 
     public int compareTo(Barcode other){
 	return toString().substring(0,6).compareTo(other.toString().substring(0,6));
     }
