4f948f1ed00e5b86b8e2e0503004d7263ff973b2
BermetKalmakova <bermetkalmakova@gmail.com>
2016-12-15 03:33:11 -0500

    Add bubbleSort

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -34,15 +34,35 @@ public class Sorts{
 	}	    
     }
 
+    public static void bubbleSort(int[] data){
+	int len = data.length-1;
+	for(int j = 0; j < len; j ++){
+	    int count = 0;
+	    for(int i = 0; i < len; i++){
+		int x = data[i];
+		if(data[i+1] < data[i]){
+		    data[i] = data[i+1];
+		    data[i+1] = x;
+		    count ++;
+		}
+	    }
+	    len--;
+	    if (count == 0){
+		break;
+	    }
+	}
+    }
+
     public static void main(String args[]){
 	int[] x = {6,3,5,1,4,2,5};
 	for(int i = 0; i < x.length;i++){
 	    System.out.println(x[i]);
 	}
-	selectionSort(x);
+	bubbleSort(x);
 	for(int i = 0; i < x.length;i++){
 	    System.out.println(x[i]);
 	}
+	//System.current time millis
     }
 
 }
